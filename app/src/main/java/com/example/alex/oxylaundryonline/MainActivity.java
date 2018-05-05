b07de8852e4d4e63c1b69938faf2ddab7b5e4020
imamfrf <imamfrf@gmail.com>
2018-05-05 16:25:04 +0700

    +splash screen

--- app/src/main/java/com/example/alex/oxylaundryonline/MainActivity.java
+++ app/src/main/java/com/example/alex/oxylaundryonline/MainActivity.java
@@ -10,18 +10,31 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout mDrawer;
     private ActionBarDrawerToggle mToggle;
+    private FirebaseAuth auth;
+    private DatabaseReference mDatabase;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         View headerView = navigationView.getHeaderView(0);
-        TextView navUsername = (TextView) headerView.findViewById(R.id.textView);
-        navUsername.setText("Your Text Here");
+        TextView navEmail = (TextView) headerView.findViewById(R.id.tv_email);
+        navEmail.setText(auth.getCurrentUser().getEmail());
+        TextView navUser = (TextView) headerView.findViewById(R.id.tv_user);
+        //navUser.setText(mDatabase.child("Users").child(auth.getCurrentUser().getUid()).addListenerForSingleValueEvent();
+
         mDrawer = (DrawerLayout) findViewById(R.id.drawer);
         mToggle = new ActionBarDrawerToggle(this, mDrawer,R.string.open,R.string.close);
         mDrawer.addDrawerListener(mToggle);
@@ -34,7 +47,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (mToggle.onOptionsItemSelected(item)){
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
---
+++ app/src/main/java/com/example/alex/oxylaundryonline/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.alex.oxylaundryonline;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    public static int timeOut = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+       // SplashScreen.this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginIntent = new Intent(SplashScreen.this, SignInActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        }, timeOut);
+    }
+}
