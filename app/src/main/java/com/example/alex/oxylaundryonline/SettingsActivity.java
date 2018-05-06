69e67514222426655a29dfdeb025c05b4b20347d
imamfrf <imamfrf@gmail.com>
2018-05-06 07:31:33 +0700

    settings, user info

---
+++ app/src/main/java/com/example/alex/oxylaundryonline/SettingsActivity.java
@@ -0,0 +1,32 @@
+package com.example.alex.oxylaundryonline;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SettingsActivity extends AppCompatActivity {
+    private TextView tv_email;
+    private FirebaseAuth auth;
+    private Button btn_change;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        tv_email = (TextView) findViewById(R.id.tv_emailSettings);
+        auth = FirebaseAuth.getInstance();
+        tv_email.setText(auth.getCurrentUser().getEmail());
+        btn_change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SettingsActivity.this, ChangeEmail.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/alex/oxylaundryonline/UserInfo.java
@@ -0,0 +1,72 @@
+package com.example.alex.oxylaundryonline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class UserInfo extends AppCompatActivity {
+    private TextView tv_namaInfo;
+    private TextView tv_telp;
+    private TextView tv_addr;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth auth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+        tv_namaInfo = (TextView)findViewById(R.id.tv_namaInfo);
+        tv_telp = (TextView)findViewById(R.id.tv_phoneInfo);
+        tv_addr = (TextView)findViewById(R.id.tv_addrInfo);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        auth = FirebaseAuth.getInstance();
+
+        mDatabase.child(auth.getCurrentUser().getUid()).child("nama").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                tv_namaInfo.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        mDatabase.child(auth.getCurrentUser().getUid()).child("nomorHP").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                tv_telp.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        mDatabase.child(auth.getCurrentUser().getUid()).child("alamat").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                tv_addr.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+    }
+}
