00c0dd7c3eea4a35a9673fa390961a96fffc8ec2
ALEX\Alex <krisnaalexander@gmail.com>
2018-10-17 17:33:38 +0700

    adapter belom jadi

--- app/src/main/java/com/example/alex/oxylaundryonline/PromoFragment.java
+++ app/src/main/java/com/example/alex/oxylaundryonline/PromoFragment.java
@@ -4,16 +4,27 @@ import android.app.Fragment;
 import android.app.FragmentContainer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class PromoFragment extends android.support.v4.app.Fragment {
+
+    private static final String TAG = "PromoFragment";
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_promo,null);
+
+
     }
+
 
 }
 
